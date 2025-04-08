272ac29b3ef7df7c5d7bb18efbf75a7043a1c814
Elijah Gervais <egervais@stevens.edu>
2025-04-08 15:22:02 -0400

    Updated Field

--- Stock.cpp
+++ Stock.cpp
@@ -5,12 +5,14 @@ class Stock{
         int stock_price;
         string company_name;
         string company_information;
+        string test_info;
 
 
     Stock(){
         this->stock_price = stock_price;
         this->company_information = company_information;
         this->company_name = company_name;
+        this->test_info = test_info;
     }
 
 };
