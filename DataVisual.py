997684e12034799ee3a3e8eb578adbd7cda10506
Elijah Gervais <egervais@stevens.edu>
2025-04-08 16:32:24 -0400

    Data Visual Changes

---
+++ DataVisual.py
@@ -0,0 +1,14 @@
+import pandas as pd  
+import numpy as np 
+import yfinance as yf 
+
+dir(yf)
+
+Tesla = yf.Ticker("Tesla")
+
+print(Tesla)
+
+df.head(Tesla)
+data = yf.download(['GOOG', 'META'],period= "1mo"
+                   
+data.head()
